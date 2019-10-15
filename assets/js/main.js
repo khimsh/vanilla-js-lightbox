669d95139519baec19919a119ba5756b6ffae2fa
khimsh <i.khimsh@gmail.com>
2019-10-15 11:30:46 +0400

    Fixed bug that returned undefined if user clicked on figure or figcaption.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,14 @@
   const modal = document.getElementById('lightbox-modal');
 
   lightbox.addEventListener('click', e => {
+    // Do nothing if user clicks on figcaption or figure tag.
+    if (
+      event.target.tagName.toLowerCase() === 'figure' ||
+      event.target.tagName.toLowerCase() === 'figcaption'
+    ) {
+      return;
+    }
+
     document.querySelector('#lightbox-modal img').src = e.target.src;
     document.querySelector('#lightbox-modal img').alt = e.target.alt;
     document.querySelector('#lightbox-modal figcaption').innerHTML =
