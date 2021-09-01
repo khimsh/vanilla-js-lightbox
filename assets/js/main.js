79d5d649dcda7eb4c5e3411384983e17926e8aa7
khimsh <i.khimsh@gmail.com>
2021-09-01 10:19:26 +0400

    Minor edits.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,57 @@
-(() => {
-  const lightbox = document.getElementById('lightbox');
+'use strict';
 
-  const createModal = (imgSrc, imgAlt) => {
-    // Create DOM elements
-    const modalElement = document.createElement('div');
-    modalElement.className = 'lightbox-modal';
+const lightbox = document.querySelector('#lightbox');
 
-    const figure = document.createElement('figure');
-    const img = document.createElement('img');
-    const figcaption = document.createElement('figcaption');
+const createModal = (imgSrc, imgAlt) => {
+  // Create DOM elements
+  const modalElement = document.createElement('div');
+  modalElement.className = 'lightbox-modal';
 
-    // Set img and figcaption attributes
-    img.src = imgSrc;
-    img.alt = imgAlt;
-    figcaption.innerText = imgAlt;
+  const figure = document.createElement('figure');
+  const img = document.createElement('img');
+  const figcaption = document.createElement('figcaption');
 
-    // Append elements to parent modalElement
-    figure.appendChild(img);
-    figure.appendChild(figcaption);
-    modalElement.appendChild(figure);
+  // Set img and figcaption attributes
+  img.src = imgSrc;
+  img.alt = imgAlt;
+  figcaption.innerText = imgAlt;
 
-    // Button element to close modal on click
-    const closeBtn = document.createElement('button');
-    closeBtn.innerHTML = `
-    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-        <title>close</title>
-        <path
-            d="M5.293 6.707l5.293 5.293-5.293 5.293c-0.391 0.391-0.391 1.024 0 1.414s1.024 0.391 1.414 0l5.293-5.293 5.293 5.293c0.391 0.391 1.024 0.391 1.414 0s0.391-1.024 0-1.414l-5.293-5.293 5.293-5.293c0.391-0.391 0.391-1.024 0-1.414s-1.024-0.391-1.414 0l-5.293 5.293-5.293-5.293c-0.391-0.391-1.024-0.391-1.414 0s-0.391 1.024 0 1.414z">
-        </path>
+  // Append elements to parent modalElement
+  figure.appendChild(img);
+  figure.appendChild(figcaption);
+  modalElement.appendChild(figure);
+
+  // Button element to close modal on click
+  const closeBtn = document.createElement('button');
+  closeBtn.innerHTML = `
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 8 8"
+    >
+      <path
+        d="M16.783,9.64l-2.5,2.5-2.5-2.5-1.5,1.5,2.5,2.5-2.5,2.5,1.5,1.5,2.5-2.5,2.5,2.5,1.5-1.5-2.5-2.5,2.5-2.5Z"
+        transform="translate(-10.283 -9.64)"
+      />
     </svg>
-    `;
+  `;
 
-    closeBtn.addEventListener('click', () => {
-      document.body.removeChild(modalElement);
-    });
+  modalElement.appendChild(closeBtn);
 
-    modalElement.appendChild(closeBtn);
-
-    return modalElement;
-  };
-
-  lightbox.addEventListener('click', e => {
-    // Do nothing if user clicks on anything other than img tag.
-    if (event.target.tagName.toLowerCase() !== 'img') {
-      return;
-    }
-
-    // Append modal to body
-    document.body.appendChild(createModal(e.target.src, e.target.alt));
+  closeBtn.addEventListener('click', () => {
+    document.body.removeChild(modalElement);
   });
-})();
+
+  return modalElement;
+};
+
+lightbox.addEventListener('click', (e) => {
+  // Do nothing if user clicks on anything other than img tag.
+  if (e.target.tagName.toLowerCase() !== 'img') {
+    return;
+  }
+
+  // Append modal to body
+  document.body.appendChild(createModal(e.target.src, e.target.alt));
+});
